08061dee99e387a45e7fbb35c1cbf489de8d0f8f
H.Hida <hokuto@sirkus.co.jp>
2025-12-26 00:20:03 +0900

    アイコン追加、クリックして追加ガイド追加

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_multiboard';
-$plugin->version = 2023112814; // Increment version
+$plugin->version = 2025122525; // Increment version
 $plugin->requires = 2022112800; // Moodle 4.1+
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v0.1.2';
+$plugin->release = 'v0.1.4';
